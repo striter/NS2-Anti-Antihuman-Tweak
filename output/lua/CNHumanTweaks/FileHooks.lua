a3407fd60bc6bcd4454ef455c2dd182749d77383
striter <letsbattle@foxmail.com>
2022-01-05 23:50:19 +0800

    Initial

---
+++ output/lua/CNHumanTweaks/FileHooks.lua
@@ -0,0 +1,2 @@
+ModLoader.SetupFileHook( "lua/DigestMixin.lua", "lua/CNHumanTweaks/DigestMixin.lua", "post" )
+ModLoader.SetupFileHook("lua/GUIDeathScreen2.lua", "lua/CNHumanTweaks/GUIDeathScreen2.lua", "replace")
